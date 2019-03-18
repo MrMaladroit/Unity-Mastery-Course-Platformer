f5a7926e4ff68d84de094f90424de869316c4eda
MrMaladroit <jamesbeckjr@gmail.com>
2019-03-17 20:40:15 -0400

    Added PlayerMovementController

---
+++ Unity Mastery Course - Platformer/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 3;
+    private float horizontal;
+    private float vertical;
+
+    private void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical);
+
+        transform.position += movement * moveSpeed * Time.fixedDeltaTime;
+    }
+}
