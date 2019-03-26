5b942c063ff75ef55f04965664a5ce026d733855
MrMaladroit <jamesbeckjr@gmail.com>
2019-03-26 11:09:15 -0400

    Setup Player Animation

--- Unity Mastery Course - Platformer/Assets/Scripts/PlayerMovementController.cs
+++ Unity Mastery Course - Platformer/Assets/Scripts/PlayerMovementController.cs
@@ -5,13 +5,26 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] private float moveSpeed = 3f;
     private float horizontal;
     private float vertical;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
 
     private void FixedUpdate()
     {
         horizontal = Input.GetAxis("Horizontal");
 
         Vector3 movement = new Vector3(horizontal, 0);
-
+        if(horizontal != 0)
+        {
+            spriteRenderer.flipX = horizontal > 0;
+        }
+        float speed = Mathf.Abs(horizontal);
+        animator.SetFloat("Speed", speed);
         transform.position += movement * moveSpeed * Time.fixedDeltaTime;
     }
 }
