809107dbde6c1ef5b33351b1a73daedcdd27360b
MrMaladroit <jamesbeckjr@gmail.com>
2019-03-26 11:41:48 -0400

    KillOnTouch + Clean up
    Set up KillOnTouch script
    Created static enemy with KOT
    Removed duplicate art files

---
+++ Unity Mastery Course - Platformer/Assets/Scripts/KillOnTouch.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerMovementController playerMovementController = collision.collider.GetComponentInChildren<PlayerMovementController>();
+        if(playerMovementController != null)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
