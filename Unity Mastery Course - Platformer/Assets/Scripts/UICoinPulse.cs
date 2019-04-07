ef33a2ab167d74f80c4ad933efa56d31cfa4fac5
MrMaladroit <jamesbeckjr@gmail.com>
2019-04-06 20:44:30 -0400

    Added Coin Animation to UI

---
+++ Unity Mastery Course - Platformer/Assets/Scripts/UICoinPulse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class UICoinPulse : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        GameManager.instance.OnCoinsChanged += HandleOnCoinsChanged;
+    }
+
+    private void HandleOnCoinsChanged(int coins)
+    {
+        animator.SetTrigger("Pulse");
+    }
+}
