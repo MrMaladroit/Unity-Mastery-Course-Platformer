ff906c0444e8ab6cfefd1d8c23b27456a1834ad6
MrMaladroit <jamesbeckjr@gmail.com>
2019-04-06 21:49:57 -0400

    Added Saw Obstacle

---
+++ Unity Mastery Course - Platformer/Assets/Scripts/Saw.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+    [SerializeField] private Transform startPosition;
+    [SerializeField] private Transform endPosition;
+    [SerializeField] private Transform sawBladeSprite;
+    private float positionPercent;
+    private int direction = 1;
+
+    private void Update()
+    {
+        positionPercent += direction * Time.deltaTime;
+        sawBladeSprite.position = Vector3.Lerp(startPosition.position, endPosition.position, positionPercent);
+
+        if (positionPercent >= 1 && direction == 1)
+            direction = -1;
+        else if (positionPercent <= 0 && direction == -1)
+            direction = 1;
+    }
+}
