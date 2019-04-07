a46015c067069da394877132f4d405f0fb1f6666
MrMaladroit <jamesbeckjr@gmail.com>
2019-04-06 22:02:25 -0400

    Refactored Saw.cs

--- Unity Mastery Course - Platformer/Assets/Scripts/Saw.cs
+++ Unity Mastery Course - Platformer/Assets/Scripts/Saw.cs
@@ -5,12 +5,16 @@ public class Saw : MonoBehaviour
     [SerializeField] private Transform startPosition;
     [SerializeField] private Transform endPosition;
     [SerializeField] private Transform sawBladeSprite;
+    [SerializeField] private float speed;
+
     private float positionPercent;
     private int direction = 1;
 
     private void Update()
     {
-        positionPercent += direction * Time.deltaTime;
+        float distance = Vector2.Distance(startPosition.position, endPosition.position);
+        float speedForDistance = speed / distance;
+        positionPercent += direction * Time.deltaTime * speedForDistance;
         sawBladeSprite.position = Vector3.Lerp(startPosition.position, endPosition.position, positionPercent);
 
         if (positionPercent >= 1 && direction == 1)
